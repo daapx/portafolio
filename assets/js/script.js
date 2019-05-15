37b84e1d8b3b4a5217cf97985f9bf3cd42cc2d54
daapx <daapx1@gmail.com>
2019-05-15 00:47:07 -0400

    agregado smooth scroll y links del navbar, navbar con position fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $(function(){
+    $("a.nav-link").click(function(event){
+        var gato = this.hash;
+        if(gato !== ""){
+            event.preventDefault;
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top - $("nav").outerHeight()
+            }, 800, function(){
+                return;
+            });
+        } else {
+            $("html, body").animate({ scrollTop: 0}, 800, function(){
+                window.location.hash = gato;
+            });
+        }
+    });
     $("[data-toggle='tooltip']").tooltip();
 });
